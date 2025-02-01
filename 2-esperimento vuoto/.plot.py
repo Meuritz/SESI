ebec77317eba1a1c59b2f38db521374d00e070ab
Meuritz <cristiano.moscetti@gmail.com>
2025-02-01 17:01:36 +0100

    corretto errori sulla seconda esercitazione

---
+++ 2-esperimento vuoto/.plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# Dati per l'asse x e l'asse y
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Creazione del plot
+plt.plot(x, y)
+
+# Aggiunta di titoli e etichette
+plt.title("Plot di base")
+plt.xlabel("Asse X")
+plt.ylabel("Asse Y")
+
+# Mostra il plot
+plt.show()
+
+
+
+
+
+
